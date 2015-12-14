27f619151d184b81effffaf708d38c2998ec0e41
root <siddhartha.mahata@gmail.com>
2015-12-14 18:39:36 +0000

    initial commit

--- src/AppBundle/Utils/UrlRESTUtil.php
+++ src/AppBundle/Utils/UrlRESTUtil.php
@@ -18,7 +18,23 @@ class UrlRESTUtil{
     
     public function getUrlList($offset, $limit, $sort){
         
-        return $this->em->getRepository('AppBundle:Url')->findBy(array(), array('id' => $sort), $limit, $offset);
+        // return $this->em->getRepository('AppBundle:Url')->findBy(array(), array('id' => $sort), $limit, $offset);
+        
+        $repository = $this->em
+            ->getRepository('AppBundle:Url');
+        
+        // createQueryBuilder automatically selects FROM AppBundle:Product
+        // and aliases it to "p"
+        $query = $repository->createQueryBuilder('p')
+            // ->where('p.price > :price')
+            // ->setParameter('price', '19.99')
+            ->orderBy('p.id', $sort)
+            ->getQuery()
+            ->useResultCache(true, 160);
+        
+        $result = $query->getResult();
+        
+        return $result;
     }
     
 }
