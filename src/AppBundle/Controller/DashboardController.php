7ef7f323f541922010e6b195dd064534a35e4dc7
root <siddhartha.mahata@gmail.com>
2016-01-02 17:33:19 +0000

    dashboard added

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('dashboard/layout.html.twig', array(
+            
+        ));
+    }
+}
